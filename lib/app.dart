5583b6f893ea412b514344595fc541d05c4c8612
Shadislam1 <shadislam999@gmail.com>
2025-06-22 11:54:37 +0600

    initial project setup

---
+++ lib/app.dart
@@ -0,0 +1,26 @@
+import 'package:crafty_bay/app_colors.dart';
+import 'package:crafty_bay/app_routes.dart';
+import 'package:crafty_bay/app_theme.dart';
+import 'package:crafty_bay/ui/screen/splash_screen.dart';
+import 'package:flutter/material.dart';
+
+class CraftyBay extends StatefulWidget {
+  const CraftyBay({super.key});
+
+  @override
+  State<CraftyBay> createState() => _CraftyBayState();
+}
+
+class _CraftyBayState extends State<CraftyBay> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+
+      initialRoute: SplashScreen.name,
+      theme:AppTheme.lightThemeData,
+      onGenerateRoute: AppRoutes.routes,
+
+    );
+
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'dart:ui';
+
+import 'package:crafty_bay/app.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/material.dart';
+
+import 'firebase_options.dart';
+
+Future<void> main() async {
+
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+    // framework error
+  FlutterError.onError = (errorDetails) {
+    FirebaseCrashlytics.instance.recordFlutterFatalError(errorDetails);
+  };
+  // framework async error
+
+  PlatformDispatcher.instance.onError = (error, stack) {
+    FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+    return true;
+  };
+
+  runApp(CraftyBay());
+}
+
